2fe51dd9fe3fec4a2db7d01c36f55c5fcec2b293
Shoummo Ahsan K <shoummoak@gmail.com>
2021-08-24 06:55:22 +0600

    Registration and Login Ready

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,9 +1,28 @@
-from django.forms import ModelForm
+from django import forms
 from .models import User
 
+# simple registration form
 # create a form with input fields which correspond to the User database fields
-class UserForm(ModelForm):
+class UserForm(forms.ModelForm):
     
     class Meta:
+
         model = User
-        fields = '__all__'
+        # these are the fields of the actual User model 
+        fields = ['name', 'email', 'username', 'password']
+        # widgets to tag classes and such to the individual form fields in the html
+        widgets = {
+            'name' : forms.TextInput(attrs={'class':'input100', 'placeholder':'Display Name'}),
+            'email' : forms.EmailInput(attrs={'class':'input100', 'placeholder':'Email'}),
+            'username' : forms.TextInput(attrs={'class':'input100', 'placeholder':'Username'}),
+            'password' : forms.PasswordInput(attrs={'class':'input100', 'placeholder':'Password'}),
+        }
+
+
+# simple login form
+class LoginForm(forms.Form):
+
+    # customize the fields with widgets just like the UserForm
+    username_or_email = forms.CharField(widget=forms.TextInput(attrs={'class':'input100', 'placeholder':'Username or Email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'input100', 'placeholder':'Password'}))
+    
